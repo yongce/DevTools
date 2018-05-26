2a6411fe3f06ff6b412b9942bae01909cdf50254
Yongce Tu <yongce.tu@gmail.com>
2018-05-27 00:53:34 +0800

    Remove root support and use AndroidLib source code directly

--- app/src/main/java/me/ycdev/android/devtools/root/cmd/RootCmdBase.java
+++ app/src/main/java/me/ycdev/android/devtools/root/cmd/RootCmdBase.java
@@ -3,7 +3,7 @@ package me.ycdev.android.devtools.root.cmd;
 import android.content.Context;
 import android.text.TextUtils;
 
-import eu.chainfire.libsuperuser.Shell;
+//import eu.chainfire.libsuperuser.Shell;
 
 public abstract class RootCmdBase extends ContextCmdBase {
     public RootCmdBase(Context cxt) {
@@ -11,15 +11,15 @@ public abstract class RootCmdBase extends ContextCmdBase {
     }
 
     public static void runSuCommand(String[] cmds, String selinuxContext) {
-        if (!TextUtils.isEmpty(selinuxContext) && isSELinuxEnforced()) {
-            String shell = Shell.SU.shell(0, selinuxContext);
-            Shell.run(shell, cmds, null, false, true);
-        } else {
-            Shell.SU.run(cmds);
-        }
+//        if (!TextUtils.isEmpty(selinuxContext) && isSELinuxEnforced()) {
+//            String shell = Shell.SU.shell(0, selinuxContext);
+//            Shell.run(shell, cmds, null, false, true);
+//        } else {
+//            Shell.SU.run(cmds);
+//        }
     }
 
-    private static boolean isSELinuxEnforced() {
-        return Shell.SU.isSELinuxEnforcing();
-    }
+//    private static boolean isSELinuxEnforced() {
+//        return Shell.SU.isSELinuxEnforcing();
+//    }
 }
